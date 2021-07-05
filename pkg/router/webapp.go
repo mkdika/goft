0ac0c16eb6419f2949b2b4394fe16f4c046b2393
Maikel Chandika <mkdika@gmail.com>
2021-07-05 20:53:41 +0700

    Update static assets binding

--- pkg/router/webapp.go
+++ pkg/router/webapp.go
@@ -9,19 +9,13 @@ import (
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
-
 	router.LoadHTMLGlob("templates/*")
-	router.Static("/assets", "assets")
+	router.Static("/assets", "./assets")
 
-	router.GET("/ping", pingHandler)
 	router.GET("/test", handler.TestHandler)
 	router.GET("/healthcheck", func(context *gin.Context) {
 		context.Status(http.StatusOK)
 	})
 
 	return router
-}
-
-func pingHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"data": "pong"})
 }
